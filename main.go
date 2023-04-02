014199249bd965042a953e05c980f004b7662488
Alfred <alfredsklotsu@gmail.com>
2023-04-02 13:25:25 +0000

    error handling improved v5.3.4

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 		AllowOrigins:     []string{"https://trains-git-main-aslotsu.vercel.app"},
 		AllowMethods:     []string{"POST", "OPTIONS", "GET", "PATCH"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "content-type"},
-		ExposeHeaders:    []string{"Origin", "Content-Length", "Content-Type", "content-type", "Set-Cookie"},
+		ExposeHeaders:    []string{"Origin", "Content-Length", "Content-Type", "content-type"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
 			return origin == "https://trains-git-main-aslotsu.vercel.app"
